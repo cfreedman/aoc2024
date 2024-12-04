0fe6fe21f5452a1c266b162279c84130a0702401
Collum <collum.freedman@gmail.com>
2024-12-04 16:01:47 -0500

    complete day4 with brute force search for matching xmas blocks in the character grid

--- day4-1.py
+++ day4-1.py
@@ -10,7 +10,26 @@ def main():
             characters.append(list(line.rstrip()))
     
     rows, cols = len(characters), len(characters[0])
-    return
+
+    total = 0
+    for row in range(rows):
+        for col in range(cols):
+            if valid_xmas_block(characters, row, col):
+                total += 1
+    print(total)
+    return total
+
+def valid_xmas_block(grid, row, col):
+    rows, cols = len(grid), len(grid[0])
+
+    if row > rows - 3 or col > cols - 3:
+        return False
+    
+    top_left_string = "".join([grid[row + i][col + i] for i in range(3)])
+    bottom_left_string = "".join([grid[row + 2 - i][col + i] for i in range(3)])
+
+    return (top_left_string == "MAS" or top_left_string == "SAM") and (bottom_left_string == "MAS" or bottom_left_string == "SAM")
+    
 
 if __name__ == "__main__":
     main()
